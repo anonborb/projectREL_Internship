5333bc949fe38d5b4e79708dddb06e01f69937b4
hylenewu <hwu3892@localhost.localdomain>
2023-01-04 16:49:09 -0500

    changes to Room.php and Basic start for Equipment.php

---
+++ public/database/Equipment.php
@@ -0,0 +1,13 @@
+
+<?php
+# Defines an Equipment object
+
+class Equipment {
+	# Components of an Equipment
+	private $eq_label;
+	private $num_of_users;
+	private $storage_space;
+
+	
+}
+?>
--- public/database/Room.php
+++ public/database/Room.php
@@ -4,19 +4,47 @@
 
 class Room {
 	# Components of a Room object
-	private $room_label;
-	private $total_capacity;
 	private $curr_capacity;
-	# insert array of equipment obj
+	private $equipment_arr;
+
 
 	# Methods
-	
-	function __construct( $room_label, $total_capacity, $curr_capacity ) {
-		$this->room_label = $room_label;
-		$this->total_capacity = $total_capacity;
-		$this->curr_capacity = $curr_capacity;
+	public function __construct(private string $room_label , private int $total_capacity) {
+		$this->curr_capacity = $total_capacity;
+		$this->equipment_arr = [];
 
 	}
 
+	# Getters for Room objects
+	public function get_room_label(): string {
+		return $this->room_label;
+	}
+
+	public function get_total_capacity()  {
+		return $this->total_capacity;
+	}
+
+	public function get_curr_capacity() {
+		return $this->curr_capacity;
+	}
+
+	# Setters 
+	public function set_room_label($room_label) {
+		$this->room_label = $room_label;
+	}
+
+	public function set_total_capacity($total_capacity) {
+		$this->total_capacity = $total_capacity;
+		
+	}
+
+
+
+	private function count_curr_capacity() {
+		# add up sum of capacity of all objects in equipment array
+		# subtract sum from total_capacity to get curr
+	}
+
+
 }	
-?>
+
