46c42a54ae06d5c53be9db6debf5b3187665b284
hylenewu <hwu3892@localhost.localdomain>
2023-01-06 16:30:32 -0500

    Installed composer and phpstan

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '76adc6c4cb6762c9ad0f907d3f0e0f0a476071d1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '76adc6c4cb6762c9ad0f907d3f0e0f0a476071d1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.9.7',
+            'version' => '1.9.7.0',
+            'reference' => '0501435cd342eac7664bd62155b1ef907fc60b6f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
